d6af0c18c951bbfb55d0311ec2866df2d614243d
banupriyarjs <banupriya.bubloo@gmail.com>
2023-11-23 02:26:11 -0500

    commit

--- CaseStudy/src/main/java/org/perscholas/casestudy/CaseStudyApplication.java
+++ CaseStudy/src/main/java/org/perscholas/casestudy/CaseStudyApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CasestudyApplication {
-
+public class CaseStudyApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CasestudyApplication.class, args);
+		SpringApplication.run(CaseStudyApplication.class, args);
 	}
 
 }
